a194de284efce4f7f63cdf03d91e53eb55844bfa
Calum Hamill-McMahon <calumhm@gmail.com>
2018-12-13 14:51:26 +0000

    Lowers Default Max speed, -completely Lowers Max & Default TurnSpeed (4000.0f -> 2.0f) -Increases DefaultShipAcceleration (from 80000.0f to 150000.0f) -decreases CRATETIMEOUT to (45 from 60.)

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,17 +6,17 @@ public static class Constants {
 	// When the game starts, a ship's maximum speed is DEFAULT...
 	//  The highest it can get is MAXIMUM...
 	public static readonly float MAXIMUMSHIPSPEED = 45.0f;
-	public static readonly float DEFAULTMAXSHIPSPEED = 24.0f;
+	public static readonly float DEFAULTMAXSHIPSPEED = 18.0f;
 
 	// When the game starts, a ship's turnspeed is DEFAULT...
 	//  The highest it can get is MAXIMUM...
-	public static readonly float MAXIMUMTURNSPEED = 120000.0f;
-	public static readonly float DEFAULTTURNSPEED = 40000.0f;
+	public static readonly float MAXIMUMTURNSPEED = 6.0f;
+	public static readonly float DEFAULTTURNSPEED = 2.0f;
 
 	// When the game starts, a ship's acceleration is DEFAULT...
 	//  The highest it can get is MAXIMUM...
 	public static readonly float MAXIMUMSHIPACCELERATION = 550000.0f;
-	public static readonly float DEFAULTSHIPACCELERATION = 80000.0f;
+	public static readonly float DEFAULTSHIPACCELERATION = 150000.0f;
 	
 	public static readonly float MAXIMUMSHIPHEALTH = 1000.0f;
 	public static readonly float MAXIMUMSHIPSHIELD = 500.0f;
@@ -25,7 +25,7 @@ public static class Constants {
     public static readonly float MAXIMUMDAMAGE = 100.0f;
 
 	// A crate disappears after this many seconds
-	public static readonly float CRATETIMEOUT = 60.0f;
+	public static readonly float CRATETIMEOUT = 45.0f;
 
     public static readonly float SHELLFORCE = 1500.0f;
 
